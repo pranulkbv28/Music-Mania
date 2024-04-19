2733c088041fa0a57fedb542247edcdaa5be32a4
pranulkbv28 <28bvpranaav@gmail.com>
2024-04-19 19:48:40 +0530

    completed hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
+const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <div className="p-4 relative z-10 w-full text-center">
+        <Spotlight
+          className="lg:-top-40 lg:left-96 lg:h-[700px] md:left-40 md:-top-10"
+          fill="white"
+        />
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master the Art of Music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you&apos;re a beginner or looking to refine
+          your skills, join us to unlock your true potential.
+        </p>
+        <div className="mt-10">
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore Courses
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
